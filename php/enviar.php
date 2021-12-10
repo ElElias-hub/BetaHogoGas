d291b86279cc728d3859813a5ed320c2b4984369
Elias Corona <56656410+ElElias-hub@users.noreply.github.com>
2021-12-10 12:38:47 -0600

    Corrección de errores

--- php/enviar.php
+++ php/enviar.php
@@ -1,18 +1,41 @@
 <?php
 
-    $destino="def00731999@gmail.com";
-    $name=$_POST["bname"];
-    $tel=$_POST["btel"];
-    $email=$_POST["bemail"];
-    $message=$_POST["txtmensaje"];
+    //$destino="def00731999@gmail.com";
+    //$name=$_POST['name'];
+    //$tel=$_POST['tel'];
+    //$email=$_POST['email'];
+    //$message=$_POST['txtmensaje'];
 
-    $asunto="CONTACTO HOGOGROUP"
-    $contenido="Nombre: " . $nombre . "\nCorreo: " . $email . "\nEnviado el " . date('d/m/Y', time() .
-    "\nTelefono: " . $tel . "\nMensaje: " . $message);
+    //$asunto="CONTACTO HOGOGROUP";
+    //$contenido="Nombre: " . $name . "\nCorreo: " . $email . "\nEnviado el " . date('d/m/Y', time()) ."\nTelefono: " . $tel . "\nMensaje: " . $message;
 
-    mail($destino,$asunto,utf8_decode($contenido));
-    header("Location:../index.html");
-    //print "<script>alert(\"Mensaje enviado.\");window.location='./index.html';</script>";
+    //mail($destino,$asunto,utf8_decode($contenido));
 
+    //print "<script>alert(\"Mensaje enviado.\");window.location='../index.html';</script>";
+
+
+    if(isset($_POST['enviar'])){
+        if(!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['tel']) && !empty($_POST['txtmensaje'])){
+
+            $destino="def00731999@gmail.com";
+            $name=$_POST['name'];
+            $tel=$_POST['tel'];
+            $email=$_POST['email'];
+            $message=$_POST['txtmensaje'];
+
+            $asunto="CONTACTO HOGOGROUP";
+            $contenido="Nombre: " . $name . "\nCorreo: " . $email . "\nEnviado el " . date('d/m/Y', time()) ."\nTelefono: " . $tel . "\nMensaje: " . $message;
+
+            $mail=mail($destino,$asunto,utf8_decode($contenido));
+
+            if($mail){
+                print "<script>alert(\"Mensaje enviado exitosamente.\");window.location='../index.html';</script>";
+            }
+            else{
+                echo "Mensaje no enviado";
+            }
+
+        }
+    }
 
 ?>
