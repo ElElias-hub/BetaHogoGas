17e8ba6e91ae6984660c343104c846c5af66fc2e
Elias Corona <56656410+ElElias-hub@users.noreply.github.com>
2021-12-10 12:58:15 -0600

    Add files via upload

--- php/enviar.php
+++ php/enviar.php
@@ -9,11 +9,15 @@
     $asunto="CONTACTO HOGOGROUP";
     $contenido="Nombre: " . $name . "\nCorreo: " . $email . "\nEnviado el " . date('d/m/Y', time()) ."\nTelefono: " . $tel . "\nMensaje: " . $message;
 
-    mail($destino,$asunto,utf8_decode($contenido));
+    $bool1=mail($destino,$asunto,utf8_decode($contenido));
 
-    print "<script>alert(\"Mensaje enviado.\");window.location='../index.html';</script>";
-
-
+    
+    if($bool1){
+        print "<script>alert(\"Mensaje enviado.\");window.location='../index.html';</script>";
+    }
+    else{
+        echo "Mensaje no enviado";
+    }
     
 
 ?>
