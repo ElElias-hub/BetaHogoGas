536fd46b79c18fa1f1b0f7b7d3fdd76a50a31f4f
Elias Corona <56656410+ElElias-hub@users.noreply.github.com>
2021-12-10 12:53:23 -0600

    Add files via upload

--- php/enviar.php
+++ php/enviar.php
@@ -1,41 +1,19 @@
 <?php
 
-    //$destino="def00731999@gmail.com";
-    //$name=$_POST['name'];
-    //$tel=$_POST['tel'];
-    //$email=$_POST['email'];
-    //$message=$_POST['txtmensaje'];
+    $destino="def00731999@gmail.com";
+    $name=$_POST['name'];
+    $tel=$_POST['tel'];
+    $email=$_POST['email'];
+    $message=$_POST['txtmensaje'];
 
-    //$asunto="CONTACTO HOGOGROUP";
-    //$contenido="Nombre: " . $name . "\nCorreo: " . $email . "\nEnviado el " . date('d/m/Y', time()) ."\nTelefono: " . $tel . "\nMensaje: " . $message;
+    $asunto="CONTACTO HOGOGROUP";
+    $contenido="Nombre: " . $name . "\nCorreo: " . $email . "\nEnviado el " . date('d/m/Y', time()) ."\nTelefono: " . $tel . "\nMensaje: " . $message;
 
-    //mail($destino,$asunto,utf8_decode($contenido));
+    mail($destino,$asunto,utf8_decode($contenido));
 
-    //print "<script>alert(\"Mensaje enviado.\");window.location='../index.html';</script>";
+    print "<script>alert(\"Mensaje enviado.\");window.location='../index.html';</script>";
 
 
-    if(isset($_POST['enviar'])){
-        if(!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['tel']) && !empty($_POST['txtmensaje'])){
-
-            $destino="def00731999@gmail.com";
-            $name=$_POST['name'];
-            $tel=$_POST['tel'];
-            $email=$_POST['email'];
-            $message=$_POST['txtmensaje'];
-
-            $asunto="CONTACTO HOGOGROUP";
-            $contenido="Nombre: " . $name . "\nCorreo: " . $email . "\nEnviado el " . date('d/m/Y', time()) ."\nTelefono: " . $tel . "\nMensaje: " . $message;
-
-            $mail=mail($destino,$asunto,utf8_decode($contenido));
-
-            if($mail){
-                print "<script>alert(\"Mensaje enviado exitosamente.\");window.location='../index.html';</script>";
-            }
-            else{
-                echo "Mensaje no enviado";
-            }
-
-        }
-    }
+    
 
 ?>
