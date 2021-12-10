a79fef985f46fc92967165485d9a3d30a4e97d17
Elias Corona <56656410+ElElias-hub@users.noreply.github.com>
2021-12-10 15:01:40 -0600

    Add files via upload

--- php/enviar.php
+++ php/enviar.php
@@ -4,20 +4,17 @@
     $name=$_POST['name'];
     $tel=$_POST['tel'];
     $email=$_POST['email'];
-    $message=$_POST['txtmensaje'];
+    $men=$_POST['mensaje'];
+
+    $cabecerasCliente  = 'MIME-Version: 1.0' . "\r\n";
+    $cabecerasCliente .= 'Content-type: text/html; charset=utf-8' . "\r\n";
 
     $asunto="CONTACTO HOGOGROUP";
-    $contenido="Nombre: " . $name . "\nCorreo: " . $email . "\nEnviado el " . date('d/m/Y', time()) ."\nTelefono: " . $tel . "\nMensaje: " . $message;
+    $contenido="Nombre: " . $name . "\nCorreo: " . $email . "\nEnviado el " . date('d/m/Y', time()) ."\nTelefono: " . $tel . "\nTelefono: ". $men ;
 
-    $bool1=mail($destino,$asunto,utf8_decode($contenido));
-
-    
-    if($bool1){
-        print "<script>alert(\"Mensaje enviado.\");window.location='../index.html';</script>";
+    $mail=mail($destino,$asunto,$contenido,$cabecerasCliente);
+    if($mail){
+        header("Location:../index.php");
     }
-    else{
-        echo "Mensaje no enviado";
-    }
-    
 
 ?>
